aaed7f829e116984246151b8f5d5c3b726d876b7
Gieandes Silva <contato@gieandessilva.com>
2017-10-02 18:47:38 -0300

    Atualização no Post do Usuário

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -29,7 +29,7 @@
                                 Posted by
 
                                 <div class="post__author-name fn">
-                                    <a href="#" class="post__author-link">Admin</a>
+                                    {{ $post->user->name }}
                                 </div>
 
                             </div>
